e5d9af05e3d328e536fb0e2c1b2f3393fe52ab9d
Danh Nguyen <congdanhnguyen116@gmail.com>
2021-08-23 09:20:22 +0700

    add heading count

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import './App.scss';
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <div className="App">
-
+      <h1 className="title">
+        Hello CoderPush
+      </h1>
+      <div className="count">
+        0
+      </div>
+      <div className="content">
+        <Card />
+      </div>
     </div>
   );
 }
