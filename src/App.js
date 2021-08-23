7621fa18fdb9e9b7dc4a576f59d598c4b36fc085
Danh Nguyen <congdanhnguyen116@gmail.com>
2021-08-23 09:39:39 +0700

    add icon card component with example data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.scss';
 import Card from './components/Card/Card'
 
 function App() {
+  const img = "https://randomuser.me/api/portraits/women/58.jpg"
+  const name = "Sara"
+  const age = "20"
   return (
     <div className="App">
       <h1 className="title">
@@ -11,7 +14,7 @@ function App() {
         0
       </div>
       <div className="content">
-        <Card />
+        <Card img={img} name={name} age={age} />
       </div>
     </div>
   );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import './style.scss'
+import '../../icons/themify-icons/themify-icons.css'
 
-const Card = () => {
+
+const Card = ({ name, age, img }) => {
     return (
-        <div>
+        <div className="card">
+            <img src={img} alt="img" className="card-img" />
+
+            <div className="card-content">
+                <span className="name">{name}</span>
+                <span className="age">{age}</span>
+            </div>
+            <div className="card-action">
+                <div className="card-btn card-btn--cancel "><i className="ti-close"></i></div>
+                <div className="card-btn card-btn--accept"><i className="ti-heart"></i></div>
+            </div>
         </div>
     )
 }
